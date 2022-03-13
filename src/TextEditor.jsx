8b603fc318a3296d88e6dec23638947484e7c02f
Kostas <kostasspanolios@gmail.com>
2022-03-13 20:17:09 +0000

    useCallback added to define wrapper div and prevent app crashing on render

--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -1,25 +1,21 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
 //NEED to sanitise whatever gets stored here (opportunity to have concrete examples on how to do this)
 //Also use this to keep track of notes in multiple files for IRP
 const TextEditor = () => {
-  const testRef = useRef(0);
-
-  useEffect(() => {
-    new Quill(
-      "#container",
-      { theme: "snow" },
-      //efergge
-      []
-    );
-  });
+  const wrapper = useCallback((wrapper) => {
+    if (wrapper === null) return;
+    wrapper.innerHTML = "";
+    let editorDiv = document.createElement("div");
+    wrapper.append(editorDiv);
+    new Quill(editorDiv, { theme: "snow" });
+  }, []);
 
   return (
-    <div>
+    <div id="container" ref={wrapper}>
       {" "}
-      <div id="container"> </div>
     </div>
   );
 };
