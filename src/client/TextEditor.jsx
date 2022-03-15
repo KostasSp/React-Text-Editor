3e906ec85a20dcf2a50c311bc0002e5a37b078fb
Kostas <kostasspanolios@gmail.com>
2022-03-15 23:03:22 +0000

    re-organise files

--- src/client/TextEditor.jsx
+++ src/client/TextEditor.jsx
@@ -2,30 +2,20 @@ import React, { useState, useEffect, useRef, useCallback } from "react";
 import Quill from "quill";
 import { io } from "socket.io-client";
 import "quill/dist/quill.snow.css";
+import toolbarOptions from "./ToolbarOptions";
 
 //NEED to sanitise whatever gets stored here (opportunity to have concrete examples on how to do this)
 //Also use this to keep track of notes in multiple files for IRP
 const TextEditor = () => {
+  console.log(toolbarOptions);
   useEffect(() => {
     const socket = io("http://localhost:3001");
 
     return () => socket.disconnect;
   }, []);
 
-  const toolbarOptions = [
-    [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    [{ font: [] }],
-    [{ list: "ordered" }, { list: "bullet" }],
-    ["bold", "italic", "underline"],
-    [{ color: [] }, { background: [] }],
-    [{ script: "sub" }, { script: "super" }],
-    [{ align: [] }],
-    ["image", "blockquote", "code-block"],
-    ["clean"],
-  ];
-
-  //useCallback to set wrapper instance (and if for some reason it is still evaluated before it is
-  //instanciated, then simply return), otherwise first render crashes app
+  //useCallback (instead of useEffect) to set wrapper instance (and if for some reason it is still
+  //evaluated before it is instantiated, then simply return), bcs otherwise first render crashes app
   const wrapper = useCallback((wrapper) => {
     if (wrapper === null) return;
     wrapper.innerHTML = "";
